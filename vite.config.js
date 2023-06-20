78ff2aba7012c257b5caccb3e89cfc4473fd7148
Simo Mafuxwana <simodms@gmail.com>
2023-06-20 20:16:25 +0200

    fix vue-tsc build error

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
+  plugins: [vue(), legacy()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+});
